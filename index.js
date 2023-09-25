93560f4e6beda28e91ff4d38ebda7033f9596556
Perog24 <perog24@gmail.com>
2023-09-25 20:43:46 +0300

    server on express

---
+++ index.js
@@ -0,0 +1,24 @@
+// const http = require('http');
+const express = require('express');
+
+const server = express();
+const PORT = 4300;
+
+server.get('/', (req, res)=>{
+res.status(200).send(`<h1 style = 'color: red'>Hello, world!</h1>
+<a href="/about" >About</a>`);
+});
+
+server.get('/about', (req, res)=>{
+res.status(200).send(`<h1>aboute page!</h1>
+<a href="/" >Home</a>`);
+});
+
+server.all('*', (req, res)=>{
+res.status(404).send(`<h1>Not found :( </h1>`);
+});
+
+
+server.listen(PORT,()=>{
+   console.log("start listening on port"+PORT);
+});
