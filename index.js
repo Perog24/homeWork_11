9071cb7f5fe36783795057fea24cc11825ebae8d
Perog24 <perog24@gmail.com>
2023-09-26 10:59:30 +0300

    final commite

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
 const express = require('express');
+const path = require('path');
 
 const server = express();
 const PORT = 4300;
 
-server.get('/', (_req, res)=>{
-res.status(200).send(`<h1 style = 'color: red'>Hello, world!</h1>
-<a href="/about" >About</a>`);
-});
+server.use(express.static(path.resolve(__dirname, 'public')));
 
-server.get('/about', (_req, res)=>{
-   
-res.status(200).send(`<h1>aboute page!</h1>
-<a href="/" >Home</a>`);
-});
-
-server.all('*', (_req, res)=>{
-res.status(404).send(`<h1>Not found :( </h1>`);
+server.all('*', (req, res)=>{
+res.status(404).sendFile(path.resolve(__dirname, 'pageErrors/404.html'));
 });
 
 
 server.listen(PORT,()=>{
-   console.log("start listening on port"+PORT);
+   console.log("start on port: "+PORT);
 });
